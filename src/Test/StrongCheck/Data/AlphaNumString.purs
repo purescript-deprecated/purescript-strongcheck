c6c6e8d7dbd7e3235fcd2c97bb0b9c890fdc084b
Gary Burgess <gary.burgess@gmail.com>
2016-06-11 03:24:41 +0100

    Fix AlphaNumString

--- src/Test/StrongCheck/Data/AlphaNumString.purs
+++ src/Test/StrongCheck/Data/AlphaNumString.purs
@@ -2,13 +2,10 @@ module Test.StrongCheck.Data.AlphaNumString where
 
 import Prelude
 
-import Data.Maybe (fromJust)
 import Data.String as S
-import Data.Traversable (sequence)
 
-import Partial.Unsafe (unsafePartial)
-
-import Test.StrongCheck.Arbitrary (class Arbitrary, class Coarbitrary, arbitrary, coarbitrary)
+import Test.StrongCheck.Arbitrary (class Arbitrary, class Coarbitrary, coarbitrary)
+import Test.StrongCheck.Gen (arrayOf, oneOf)
 
 newtype AlphaNumString = AlphaNumString String
 
@@ -19,18 +16,10 @@ derive instance eqAlphaNumString :: Eq AlphaNumString
 derive instance ordAlphaNumString :: Ord AlphaNumString
 
 instance arbAlphaNumString :: Arbitrary AlphaNumString where
-  arbitrary = unsafePartial do
-    arrNum <- arbitrary
-    pure $ fromJust $ (AlphaNumString <<< S.fromCharArray) <$> sequence (lookup <$> arrNum)
+  arbitrary = AlphaNumString <<< S.fromCharArray <$> arrayOf anyChar
     where
-    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    lookup x = S.charAt index chars
-      where
-      index =
-        let
-          i1 = S.length chars - 1
-          i2 = x * S.length chars
-        in if i1 < i2 then i1 else i2
+    rest = S.toCharArray "bcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    anyChar = oneOf (pure 'a') (map pure rest)
 
 instance coarbAlphaNumString :: Coarbitrary AlphaNumString where
   coarbitrary (AlphaNumString s) = coarbitrary s
