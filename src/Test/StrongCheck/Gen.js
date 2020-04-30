3e6505270ada04550998d3a62607e04ad488a027
Thomas R. Honeyman <thoneyman@awakesecurity.com>
2020-04-30 15:23:39 -0700

    Remove files except deprecation notice

--- src/Test/StrongCheck/Gen.js
+++
@@ -1,7 +0,0 @@
-exports.float32ToInt32 = function(n) {
-    var arr = new ArrayBuffer(4),
-        fv = new Float32Array(arr),
-        iv = new Int32Array(arr);
-    fv[0] = n;
-    return iv[0];
-};
